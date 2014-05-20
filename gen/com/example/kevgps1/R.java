638abd50868cb7252db7f4db0c8852327d9d5f69
kevinwalsh <kevface91@gmail.com>
2014-05-20 11:09:59 +0100

    - Ver1.0, kinda works but i wanna rejig everything. wanna clean up main. wherever we collect data (essentially just location, maybe time), im gonna declare a global var in relevant class, and "return" it in a unique method. im gonna call this method from main to "import" latest value. - im gonna then be able to export vals as necessary using the arraylist of locations. lastly im gonna try have a separate class to update ALL textviews? like jacks updateText

--- gen/com/example/kevgps1/R.java
+++ gen/com/example/kevgps1/R.java
@@ -26,13 +26,14 @@ public final class R {
     }
     public static final class id {
         public static final int TextView2=0x7f080003;
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000b;
         public static final int button1=0x7f080001;
         public static final int tableRow1=0x7f080005;
-        public static final int tableRow2=0x7f080007;
-        public static final int tableRow3=0x7f080008;
-        public static final int tableRow4=0x7f080009;
-        public static final int tableRow5=0x7f080006;
+        public static final int tableRow2=0x7f080006;
+        public static final int tableRow3=0x7f080007;
+        public static final int tableRow4=0x7f080008;
+        public static final int tableRow5=0x7f080009;
+        public static final int tableRow6=0x7f08000a;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
         public static final int textView3=0x7f080004;
